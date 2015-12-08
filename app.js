7ddbecaf30283e0cf2cbe2be4e60caa7559cc1e1
Gabriel Malet <gabrielmalet@gmail.com>
2015-12-08 19:31:09 +0100

    Clean node application

--- app.js
+++ app.js
@@ -4,20 +4,23 @@ var express = require('express');
 var http = require('http');
 var path = require('path');
 var bodyParser = require('body-parser');
-var app = express();
+var jwt = require("jsonwebtoken");
 var config = require('./config.json');
 var argv = require('yargs').argv;
 var fs = require('fs');
 var mongoose = require('mongoose');
+var app = express();
 
-var server = http.createServer(app);
-var io = require('socket.io')(server);
-
-// default
-var env = process.env.NODE_ENV || argv.env;
 var port = 3000, host = 'localhost';
 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
+  next();
+});
 app.use(express.static(config.path.public));
 
 // db
@@ -25,7 +28,7 @@ mongoose.connect(config.mongo.uri);
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'Connection error:'));
-db.once('open', function (callback) { console.log('Connection OK.'); });
+db.once('open', function (cb) { console.log('Connection OK.'); });
 
 // Add models
 fs.readdirSync(config.path.server.model).forEach(function (file) {
@@ -34,17 +37,14 @@ fs.readdirSync(config.path.server.model).forEach(function (file) {
   }
 });
 
-require(config.path.server.socket)(io, config);
-
 // Add routes from controller file
 fs.readdirSync(config.path.server.controller).forEach(function (file) {
   if(file.substr(-3) == '.js') {
-    require(config.path.server.controller + '/' + file).controller(app, io, config);
+    require(config.path.server.controller + '/' + file).controller(app, config);
   }
 });
 
 
-server.listen(port, host);
-server.on('listening', function() {
-  console.log('Express server started on port %s at %s', server.address().port, server.address().address);
+app.listen(port, function() {
+  console.log('Express server started on port %s', port);
 });
