c66ea8c011d35f5687ecd2e9b93bd06c3da98de2
Gabriel Malet <gabrielmalet@gmail.com>
2015-12-09 12:11:24 +0100

    Delete

--- src/client/app/js/directives/index.js
+++ src/client/app/js/directives/index.js
@@ -5,4 +5,3 @@ var app = require('angular').module('otr.directives', []);
 app.directive('sideNavigation', require('./sideNavigation'));
 app.directive('minimizeSidebar', require('./minimizeSidebar'));
 app.directive('slimScroll', require('./slimScroll'));
-app.directive('chatSlimScroll', require('./chatSlimScroll'));
