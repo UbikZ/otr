805c5b2a335b9b4e8c4b5b26d4a9fde16b137344
Gabriel Malet <gabrielmalet@gmail.com>
2015-12-09 19:57:03 +0100

    Add ontime oneshot information

--- src/client/app/js/controllers/ontime.js
+++ src/client/app/js/controllers/ontime.js
@@ -7,7 +7,7 @@ module.exports = ['$scope', '$rootScope', 'authService',
     $scope.loading = true;
     $scope.json = undefined;
     authService.meOntime(function (res) {
-      $scope.json = res;
+      $scope.json = JSON.stringify(res, null, 4);
       $scope.loading = false;
     }, function (err) {
       toastr.error(err.message);
