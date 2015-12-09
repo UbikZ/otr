4583525b3602ec29a8284dfad19598101ec5cd0e
Gabriel Malet <gabrielmalet@gmail.com>
2015-12-09 20:32:36 +0100

    Add user angular feature

--- src/client/app/js/controllers/index.js
+++ src/client/app/js/controllers/index.js
@@ -8,6 +8,7 @@ app.controller('login.controller', require('./login'));
 app.controller('home.controller', require('./home'));
 app.controller('translate.controller', require('./translate'));
 app.controller('ontime.controller', require('./ontime'));
+app.controller('user.controller', require('./user'));
 
 // Form controllers
 app.controller('form.profile.controller', require('./forms/profile'));
--- src/client/app/js/services/user.js
+++ src/client/app/js/services/user.js
@@ -2,6 +2,7 @@
 
 var angular = require('angular');
 var env = require('../env');
+var jquery = require('jquery');
 
 module.exports = ['$http',
   function($http) {
@@ -11,6 +12,10 @@ module.exports = ['$http',
       update: function(data, success, error) {
         $http.post(baseUrl + '/user/update', data).success(success).error(error)
       },
+      get: function(data, success, error) {
+        var url = baseUrl + '/user?' + jquery.param(data);
+        $http.get(url).success(success).error(error)
+      }
     };
   }
 ];
