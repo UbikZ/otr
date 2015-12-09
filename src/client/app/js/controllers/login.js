2d3cb8f43f2e49b0451aa6e5910a9187cdd03df6
Gabriel Malet <gabrielmalet@gmail.com>
2015-12-09 17:07:14 +0100

    Add spinner

--- src/client/app/js/controllers/login.js
+++ src/client/app/js/controllers/login.js
@@ -4,14 +4,19 @@ var toastr = require('toastr');
 
 module.exports = ['$scope', '$rootScope', 'authService', '$location', '$localStorage',
   function ($scope, $rootScope, authService, $location, $localStorage) {
+    $scope.loading = false;
+
     $scope.login = function (user) {
+      $scope.loading = true;
       authService.login(user, function (res) {
+        $scope.loading = false;
         $localStorage.token = res.user.identity.token;
         $localStorage.ot_token = res.user.identity.ontime_token;
         $rootScope.user = res.user;
         $rootScope.isAuthenticated = true;
         $location.path('/');
       }, function (err) {
+        $scope.loading = false;
         toastr.error(err.message);
       });
     };
