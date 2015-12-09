520a02e6de039d37e15ea6d8a11d5e1123c16abd
Gabriel Malet <gabrielmalet@gmail.com>
2015-12-09 16:40:36 +0100

    Clean authentication angular service

--- src/client/app/js/services/auth.js
+++ src/client/app/js/services/auth.js
@@ -6,40 +6,6 @@ module.exports = ['$http', '$localStorage',
   function($http, $localStorage) {
     var baseUrl = "http://localhost:3000/api/v1";
 
-    function changeUser(user) {
-      angular.extend(currentUser, user);
-    }
-
-    function urlBase64Decode(str) {
-      var output = str.replace('-', '+').replace('_', '/');
-      switch (output.length % 4) {
-        case 0:
-          break;
-        case 2:
-          output += '==';
-          break;
-        case 3:
-          output += '=';
-          break;
-        default:
-          throw 'Illegal base64url string!';
-      }
-
-      return window.atob(output);
-    }
-
-    function getUserFromToken() {
-      var token = $localStorage.token;
-      var user = {};
-      if (typeof token !== 'undefined') {
-        var encoded = token.split('.')[1];
-        user = JSON.parse(urlBase64Decode(encoded));
-      }
-      return user;
-    }
-
-    var currentUser = getUserFromToken();
-
     return {
       authenticate: function(data, success, error) {
         $http.post(baseUrl + '/authenticate', data).success(success).error(error)
@@ -50,11 +16,9 @@ module.exports = ['$http', '$localStorage',
       me: function(success, error) {
         $http.get(baseUrl + '/me/').success(success).error(error)
       },
-      logout: function(success) {
-        changeUser({});
-        delete $localStorage.token;
-        success();
-      }
+      meOntime: function(success, error) {
+        $http.get(baseUrl + '/me-ontime/').success(success).error(error)
+      },
     };
   }
 ];
