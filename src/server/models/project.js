6ee7c45b1e1073f301c41e07e28441f0b4f890f2
Gabriel Malet <gabrielmalet@gmail.com>
2015-12-10 10:57:40 +0100

    Update model

--- src/server/models/project.js
+++ src/server/models/project.js
@@ -5,8 +5,7 @@ var mongoose = require('mongoose');
 module.exports = (function(){
   var schema = new mongoose.Schema({
     name: String,
-    description: String,
-    logo: String,
+    priority: { type: Number, min: 0, max: 2 },
     creation: {
       user: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
       date: Date
@@ -19,5 +18,5 @@ module.exports = (function(){
     settings: {type: mongoose.Schema.Types.ObjectId, ref: 'Setting'}
   });
 
-  return mongoose.model('Organization', schema);
+  return mongoose.model('Project', schema);
 })();
---
+++ src/server/models/setting.js
@@ -0,0 +1,40 @@
+'use strict';
+
+var mongoose = require('mongoose');
+
+module.exports = (function(){
+  var schema = new mongoose.Schema({
+    project_dev: {
+      contributor_price: { type: Number, min: 0 },
+      contributor_occupation: { type: Number, min: 0, max: 100 },
+    },
+    project_management: {
+      scrummaster_price: { type: Number, min: 0 },
+      scrummaster_occupation: { type: Number, min: 0, max: 100 },
+    },
+    billing: {
+      show: Boolean,
+      show_dev_price: Boolean,
+      show_management_price: Boolean,
+      rate_multiplier: { type: Number, min: 0 },
+      final_estimate: Boolean,
+    },
+    unit: {
+      label: String,
+      type: { type: Number, min: 0, max: 2 }, // 0: minutes | 1: hours | 2: days
+    },
+    date: {
+      show: Boolean,
+      format: String,
+      start_date: {type: Date, default: Date.now },
+    },
+    iteration: {
+      contributor_available: { type: Number, min: 0 },
+      hour_per_day: { type: Number, min: 0 },
+      day_per_week: { type: Number, min: 0 },
+      week_per_iteration: { type: Number, min: 0 },
+    }
+  });
+
+  return mongoose.model('Setting', schema);
+})();
