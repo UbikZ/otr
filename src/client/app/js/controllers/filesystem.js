3b1a5625159ef640c80a5da5f3c712decfb50773
Gabriel Malet <gabrielmalet@gmail.com>
2015-12-11 15:50:16 +0100

    Filesystem improvements

--- src/client/app/js/controllers/filesystem.js
+++ src/client/app/js/controllers/filesystem.js
@@ -13,16 +13,16 @@ module.exports = ['$scope', '$rootScope', 'organizationService',
     $scope.dataForTheTree =
       [
         { "name" : "Joe", "age" : "21", "type": "folder", "children" : [
-          { "name" : "Smith", "age" : "42", "children" : [] },
-          { "name" : "Gary", "age" : "21", "children" : [
-            { "name" : "Jenifer", "age" : "23", "children" : [
-              { "name" : "Dani", "age" : "32", "children" : [] },
-              { "name" : "Max", "age" : "34", "children" : [] }
+          { "name" : "Smith", "age" : "42", type: "file"},
+          { "name" : "Gary", "age" : "21", "type": "folder", "children" : [
+            { "name" : "Jenifer", "age" : "23", "type": "folder", "children" : [
+              { "name" : "Dani", "age" : "32", type: "file" },
+              { "name" : "Max", "age" : "34", type: "file" }
             ]}
           ]}
         ]},
-        { "name" : "Albert", "age" : "33", "children" : [] },
-        { "name" : "Ron", "age" : "29", "children" : [] }
+        { "name" : "Albert", "age" : "33", type: "file" },
+        { "name" : "Ron", "age" : "29", type: "file" }
       ];
   }
 ];
