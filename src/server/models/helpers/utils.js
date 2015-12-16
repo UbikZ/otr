478180a8e0d5e59dfcd0a2dde3ac8e3256bbb932
Gabriel Malet <gabrielmalet@gmail.com>
2015-12-16 14:42:41 +0100

    Add check in rescursive function

--- src/server/models/helpers/utils.js
+++ src/server/models/helpers/utils.js
@@ -1,13 +1,15 @@
 'use strict';
 
 function findRecursivelyById(parentElement, attributeName, elementId, cb) {
-  var element = parentElement[attributeName].id(elementId);
-  if (element == undefined) {
-    parentElement[attributeName].forEach(function (subElement) {
-      findRecursivelyById(subElement, attributeName, elementId, cb);
-    });
-  } else {
-    cb(element);
+  if (parentElement[attributeName] != undefined) {
+    var element = parentElement[attributeName].id(elementId);
+    if (element == undefined) {
+      parentElement[attributeName].forEach(function (subElement) {
+        findRecursivelyById(subElement, attributeName, elementId, cb);
+      });
+    } else {
+      cb(element);
+    }
   }
 };
 
