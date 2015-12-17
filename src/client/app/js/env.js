1f917f4094c3d014d6f0f5ae9b61338346a939da
Gabriel Malet <gabrielmalet@gmail.com>
2015-12-17 15:58:54 +0100

    Change API url and add translations for login form

--- src/client/app/js/env.js
+++ src/client/app/js/env.js
@@ -3,7 +3,7 @@
 module.exports = {
   appName: 'otr',
   baseTemplateDir: '/views/',
-  apiUrl: 'http://localhost:3000/api/v1',
+  apiUrl: '/api/v1',
 
   templatePath: function (view) { return [this.baseTemplateDir, view].join(''); }
 };
