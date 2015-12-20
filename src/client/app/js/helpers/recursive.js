e810a87b1ff38e631594f5d64bc78a395f7c809e
Gabriel Malet <gabrielmalet@gmail.com>
2015-12-20 20:45:42 +0100

    Add check of undefined values

--- src/client/app/js/helpers/recursive.js
+++ src/client/app/js/helpers/recursive.js
@@ -18,15 +18,17 @@ function convertTreeView(parent) {
 
 function findRecursivelyById(parentElement, attributeName, elementId, getParent, cb, passFirstElement) {
   var elements = passFirstElement === true ? parentElement : parentElement[attributeName];
-  elements.forEach(function (subElement) {
-    if (subElement['_id'] != elementId) {
-      findRecursivelyById(subElement, attributeName, elementId, getParent, cb);
-    } else if (parentElement.length === undefined) {
-      cb(getParent === true ? parentElement : subElement);
-    } else {
-      cb(getParent === true ? undefined : subElement);
-    }
-  });
+  if (elements != undefined) {
+    elements.forEach(function (subElement) {
+      if (subElement['_id'] != elementId) {
+        findRecursivelyById(subElement, attributeName, elementId, getParent, cb);
+      } else if (parentElement.length === undefined) {
+        cb(getParent === true ? parentElement : subElement);
+      } else {
+        cb(getParent === true ? undefined : subElement);
+      }
+    });
+  }
 }
 
 // todo: breadcrumb feature here
