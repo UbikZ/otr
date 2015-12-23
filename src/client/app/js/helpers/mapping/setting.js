61426ad485f2caf43ccb33956e90299abf6d8852
Gabriel Malet <gabrielmalet@gmail.com>
2015-12-23 12:23:56 +0100

    Fix mapping function

--- src/client/app/js/helpers/mapping/setting.js
+++ src/client/app/js/helpers/mapping/setting.js
@@ -1,39 +1,76 @@
 'use strict';
 
-function dalToDTO(setting) {
+function check(item, cb) {
+  if (item != undefined) {
+    cb(item);
+  }
+}
+
+// Overkill callbacks
+function dalToDTO(item) {
   var result = {};
 
-  if (setting != undefined) {
+  check(item, function(setting) {
     result.id = setting.id;
 
-    if (setting.project_dev != undefined) {
-      result.contributorPrice = setting.project_dev.contributor_price;
-      result.contributorOccupation = setting.project_dev.contributor_occupation;
-    }
-    if (setting.project_management != undefined) {
-      result.scrummasterPrice = setting.project_management.scrummaster_price;
-      result.scrummasterOccupation = setting.project_management.scrummaster_occupation;
-    }
-    if (setting.billing != undefined) {
-      result.rateMultiplier = setting.billing.rate_multiplier;
-      result.showDev = setting.billing.show_dev_price;
-      result.showManagement = setting.billing.show_management_price;
-    }
-    if (setting.unit != undefined) {
-      result.estimateType = setting.unit.estimate_type;
-      result.rangeEstimateUnit = setting.unit.range_estimate_unit;
-      result.label = setting.unit.label;
-    }
-    if (setting.date != undefined) {
-      result.showDate = setting.date.show;
-    }
-    if (setting.iteration != undefined) {
-      result.contributorAvailable = setting.iteration.contributor_available;
-      result.hourPerDay = setting.iteration.hour_per_day;
-      result.dayPerWeek = setting.iteration.day_per_week;
-      result.weekPerIteration = setting.iteration.week_per_iteration;
-    }
-  }
+    check(setting.project_dev, function(project_dev) {
+      check(project_dev.contributor_price, function(contributor_price) {
+        result.contributorPrice = contributor_price;
+      });
+      check(project_dev.contributor_occupation, function(contributor_occupation) {
+        result.contributorOccupation = contributor_occupation;
+      });
+    });
+    check(setting.project_management, function(project_management) {
+      check(project_management.scrummaster_price, function(scrummaster_price) {
+        result.scrummasterPrice = scrummaster_price;
+      });
+      check(project_management.scrummaster_occupation, function(scrummaster_occupation) {
+        result.scrummasterOccupation = scrummaster_occupation;
+      });
+    });
+    check(setting.billing, function(billing) {
+      check(billing.rate_multiplier, function(rate_multiplier) {
+        result.rateMultiplier = rate_multiplier;
+      });
+      check(billing.show_dev_price, function(show_dev_price) {
+        result.showDev = show_dev_price;
+      });
+      check(billing.show_management_price, function(show_management_price) {
+        result.showManagement = show_management_price;
+      });
+    });
+    check(setting.unit, function(unit) {
+      check(unit.estimate_type, function(estimate_type) {
+        result.estimateType = estimate_type;
+      });
+      check(unit.range_estimate_unit, function(range_estimate_unit) {
+        result.rangeEstimateUnit = range_estimate_unit;
+      });
+      check(unit.label, function(label) {
+        result.label = label;
+      });
+    });
+    check(setting.date, function(date) {
+      check(date.show, function(show) {
+        result.showDate = show;
+      });
+    });
+    check(setting.iteration, function(iteration) {
+      check(iteration.contributor_available, function(contributor_available) {
+        result.contributorAvailable = contributor_available;
+      });
+      check(iteration.hour_per_day, function(hour_per_day) {
+        result.hourPerDay = hour_per_day;
+      });
+      check(iteration.day_per_week, function(day_per_week) {
+        result.dayPerWeek = day_per_week;
+      });
+      check(iteration.week_per_iteration, function(week_per_iteration) {
+        result.weekPerIteration = week_per_iteration;
+      });
+    });
+  });
 
   return result;
 }
