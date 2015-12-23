b94414c2dfee52fe56d0b48177bcda8c169e52e4
Gabriel Malet <gabrielmalet@gmail.com>
2015-12-23 10:58:59 +0100

    Add check value

--- src/client/app/js/helpers/mapping/setting.js
+++ src/client/app/js/helpers/mapping/setting.js
@@ -1,24 +1,37 @@
 'use strict';
 
 function dalToDTO(setting) {
-  return {
-    id: setting.id,
-    contributorPrice: setting.project_dev.contributor_price,
-    contributorOccupation: setting.project_dev.contributor_occupation,
-    scrummasterPrice: setting.project_management.scrummaster_price,
-    scrummasterOccupation: setting.project_management.scrummaster_occupation,
-    rateMultiplier: setting.billing.rate_multiplier,
-    showDev: setting.billing.show_dev_price,
-    showManagement: setting.billing.show_management_price,
-    estimateType: setting.unit.estimate_type,
-    rangeEstimateUnit: setting.unit.range_estimate_unit,
-    label: setting.unit.label,
-    showDate: setting.date.show,
-    contributorAvailable: setting.iteration.contributor_available,
-    hourPerDay: setting.iteration.hour_per_day,
-    dayPerWeek: setting.iteration.day_per_week,
-    weekPerIteration: setting.iteration.week_per_iteration,
-  };
+  var result = {id: setting.id};
+
+  if (setting.project_dev != undefined) {
+    result.contributorPrice = setting.project_dev.contributor_price;
+    result.contributorOccupation = setting.project_dev.contributor_occupation;
+  }
+  if (setting.project_management != undefined) {
+    result.scrummasterPrice = setting.project_management.scrummaster_price;
+    result.scrummasterOccupation = setting.project_management.scrummaster_occupation;
+  }
+  if (setting.billing != undefined) {
+    result.rateMultiplier = setting.billing.rate_multiplier;
+    result.showDev = setting.billing.show_dev_price;
+    result.showManagement = setting.billing.show_management_price;
+  }
+  if (setting.unit != undefined) {
+    result.estimateType = setting.unit.estimate_type;
+    result.rangeEstimateUnit = setting.unit.range_estimate_unit;
+    result.label = setting.unit.label;
+  }
+  if (setting.date != undefined) {
+    result.showDate = setting.date.show;
+  }
+  if (setting.iteration != undefined) {
+    result.contributorAvailable = setting.iteration.contributor_available;
+    result.hourPerDay = setting.iteration.hour_per_day;
+    result.dayPerWeek = setting.iteration.day_per_week;
+    result.weekPerIteration = setting.iteration.week_per_iteration;
+  }
+
+  return result;
 }
 
 module.exports = {
