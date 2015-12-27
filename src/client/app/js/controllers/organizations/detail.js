c19f55e75b08f44296f9952c2ca9db7954e8590e
Gabriel Malet <gabrielmalet@gmail.com>
2015-12-27 17:39:18 +0100

    Manage error with bad organization

--- src/client/app/js/controllers/organizations/detail.js
+++ src/client/app/js/controllers/organizations/detail.js
@@ -2,11 +2,15 @@
 
 var toastr = require('toastr');
 
-module.exports = ['$scope', '$rootScope', '$stateParams', 'organizationService', '$uibModal',
-  function ($scope, $rootScope, $stateParams, organizationService, $uibModal) {
+module.exports = ['$scope', '$rootScope', '$stateParams', 'organizationService', '$uibModal', '$location',
+  function ($scope, $rootScope, $stateParams, organizationService, $uibModal, $location) {
     organizationService.get({id: $stateParams.id}, function (res) {
-      $scope.organization = res.organizations[0];
-      $scope.$broadcast('load-organization', {organization: $scope.organization});
+      if (res.organizations.length == 0) {
+        $location.path('/');
+      } else {
+        $scope.organization = res.organizations[0];
+        $scope.$broadcast('load-organization', {organization: $scope.organization});
+      }
     });
 
     $scope.edit = function (objectId) {
