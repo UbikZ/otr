23042cb971ab26a395078c8c144af537e1fdfe80
Gabriel Malet <gabrielmalet@gmail.com>
2016-01-03 14:48:32 +0100

    Keep selected language for template change

--- src/client/app/js/controllers/translate.js
+++ src/client/app/js/controllers/translate.js
@@ -3,6 +3,7 @@
 module.exports = ['$scope', '$translate',
   function($scope, $translate) {
     $scope.changeLanguage = function (langKey) {
+      $scope.currentLanguage = langKey;
       $translate.use(langKey);
     };
   }
