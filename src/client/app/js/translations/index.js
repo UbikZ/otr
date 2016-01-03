381ff5fe1e12441e5f27d2ff941663da7ddba7c8
Gabriel Malet <gabrielmalet@gmail.com>
2016-01-03 14:48:04 +0100

    Change famous language

--- src/client/app/js/translations/index.js
+++ src/client/app/js/translations/index.js
@@ -4,6 +4,6 @@ module.exports = function($translateProvider) {
   $translateProvider.translations('en', require('./en.json'));
   $translateProvider.translations('fr', require('./fr.json'));
 
-  $translateProvider.preferredLanguage('en');
+  $translateProvider.preferredLanguage('fr');
 };
 
