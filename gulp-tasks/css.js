e89e2533f2fb9345b576f281c121270591d2b39e
Gabriel Malet <gabrielmalet@gmail.com>
2016-01-06 12:08:13 +0100

    Add pre-processing task

--- gulp-tasks/css.js
+++ gulp-tasks/css.js
@@ -1,12 +1,13 @@
 'use strict';
 
-module.exports = function(gulp, plugins, npmPackages, config) {
-  return function() {
+module.exports = function (gulp, plugins, npmPackages, config) {
+  return function () {
     var printStream = gulp.src([
-        config.path.client.app + '/css/main.css',
-        config.path.client.print + '/css/**/*.css',
-        config.path.public + '/lib/font-awesome/dist/css/**/*.css'
-      ]).pipe(plugins.concatCss('print.min.css', { rebaseUrls: false }));
+      config.path.public + '/lib/font-awesome/dist/css/**/*.css',
+      config.path.public + '/lib/bootstrap/dist/css/**/*.css',
+      config.path.client.app + '/css/{main,own}.css',
+      config.path.client.print + '/css/**/*.css',
+    ]).pipe(plugins.concatCss('print.min.css', {rebaseUrls: false}));
 
     if (!config.env.debug) {
       printStream.pipe(plugins.minifyCss());
@@ -15,7 +16,7 @@ module.exports = function(gulp, plugins, npmPackages, config) {
     printStream.pipe(gulp.dest(config.path.public + '/dist'));
 
     var stream = gulp.src([config.path.public + '/lib/**/*.css', 'src/client/app/css/**/*.css'])
-      .pipe(plugins.concatCss('app.min.css', { rebaseUrls: false }));
+      .pipe(plugins.concatCss('app.min.css', {rebaseUrls: false}));
 
     if (!config.env.debug) {
       stream.pipe(plugins.minifyCss());
