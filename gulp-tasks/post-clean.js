940d5d072202ec1333a19e7ced1057bc21e2912f
Gabriel Malet <gabrielmalet@gmail.com>
2016-01-13 21:59:02 +0100

    Finalize

--- gulp-tasks/post-clean.js
+++ gulp-tasks/post-clean.js
@@ -2,6 +2,6 @@
 
 module.exports = function(gulp, plugins, npmPackages, config) {
   return function() {
-    plugins.del([config.path.public + '/lib'/*, config.path.client.app + '/dist'*/]);
+    plugins.del([config.path.public + '/lib', config.path.client.app + '/dist']);
   };
 };
