a6efecbeadf8d99b5c3e60c9bf7cdb83c1828538
Gabriel Malet <gabrielmalet@gmail.com>
2016-01-14 18:12:45 +0100

    Remove useless fonts, change tasks

--- gulp-tasks/revision.js
+++ gulp-tasks/revision.js
@@ -4,22 +4,18 @@ var fs = require('fs');
 
 module.exports = function (gulp, plugins, npmPackages, config) {
   return function () {
-    var manifest = require('../' + config.path.public + '/dist/rev-manifest.json');
+      fs.readFile(config.path.client.app + '/views/index.html', 'utf8', function (err, data) {
+        if (err) {
+          return console.log(err);
+        }
+        if (!config.env.debug) {
+          var manifest = require('../' + config.path.public + '/dist/rev-manifest.json');
+          for (var key in manifest) {
+            data = data.replace(key, manifest[key]);
+          }
+        }
 
-    fs.readFile(config.path.client.app + '/views/index.html', 'utf8', function (err, data) {
-      if (err) {
-        return console.log(err);
-      }
-      for (var key in manifest) {
-        data = data.replace(key, manifest[key]);
-      }
-
-      fs.writeFile(config.path.public + '/index.html', data);
-    });
-
-    /*return gulp.src([config.path.public + '/dist/rev-manifest.json', config.path.client.app + '/views/index.html'])
-     .pipe(plugins.ifProd(plugins.revCollector({replaceReved: true})))
-     .pipe(gulp.dest(config.path.public))
-     ;*/
+        fs.writeFile(config.path.public + '/index.html', data);
+      });
   };
 };
--- src/server/controllers/index.js
+++ src/server/controllers/index.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var path = require('path');
+
 module.exports.controller = function (app, config) {
   app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, config.path.public + '/index.html'));
