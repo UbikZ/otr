cec5473ea9fecd1481fddbb2942761e350de30c4
Gabriel Malet <gabrielmalet@gmail.com>
2016-01-15 16:23:31 +0100

    try to fix bg

--- scripts/staging/post-deploy.sh
+++ scripts/staging/post-deploy.sh
@@ -2,4 +2,5 @@
 
 npm install
 gulp pre-clean && gulp install --env=staging && \
-node app.js --env=staging& && PID=$? && mocha ./test && pkill -9 $PID
+`node app.js --env=staging &` \
+&& PID=$? && mocha ./test && pkill -9 $PID
