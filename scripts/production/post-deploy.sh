b459e09189220bcfe028a4313f9fa1a2c0af435a
Gabriel Malet <gabrielmalet@gmail.com>
2016-01-17 00:21:18 +0100

    Fix run scripts

--- scripts/production/post-deploy.sh
+++ scripts/production/post-deploy.sh
@@ -3,4 +3,4 @@
 NODE_PATH=./node_modules/.bin
 
 npm install
-export NODE_ENV=production && npm build && pm2 startOrRestart ecosystem.json
+export NODE_ENV=production && npm run build && pm2 startOrRestart ecosystem.json
