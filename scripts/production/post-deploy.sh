c29bd23511dcdb5402ee56e00ae54e08712d6310
Gabriel Malet <gabrielmalet@gmail.com>
2016-01-16 23:23:19 +0100

    Add coverage

--- scripts/production/post-deploy.sh
+++ scripts/production/post-deploy.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
+NODE_PATH=./node_modules/.bin
+
 npm install
 export NODE_ENV=production && \
-gulp pre-clean && gulp install && \
+$NODE_PATH/gulp pre-clean && NODE_PATH/gulp install && \
 pm2 startOrRestart ecosystem.json
--- scripts/staging/post-deploy.sh
+++ scripts/staging/post-deploy.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
+NODE_PATH=./node_modules
+
 npm install
 export NODE_ENV=staging && \
-gulp pre-clean && gulp install && \
-mocha ./test
+$NODE_PATH/.bin/gulp pre-clean && $NODE_PATH/.bin/gulp install && \
+$NODE_PATH/.bin/istanbul cover $NODE_PATH/mocha/bin/_mocha test
