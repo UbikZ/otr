ec3e664462c35753c3eb62a82457666ffb9d50a9
Gabriel Malet <gabrielmalet@gmail.com>
2016-01-18 18:22:40 +0100

    Comment hack code

--- src/server/middleware.js
+++ src/server/middleware.js
@@ -28,6 +28,8 @@ module.exports = function (app, config) {
 
   app.use(express.static(config.path.public));
 
+  /*
+  // Hack code
   app.use(function (req, res, next) {
     var _send = res.send;
     var sent = false;
@@ -37,7 +39,7 @@ module.exports = function (app, config) {
       sent = true;
     };
     next();
-  });
+  });*/
 
   // Database configuration
   mongoose.connect(config.env[process.env.NODE_ENV].mongo.uri);
