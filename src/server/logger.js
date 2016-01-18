fa92c8e4f472d7121e2634e6232fd2a3b4b8c0cb
Gabriel Malet <gabrielmalet@gmail.com>
2016-01-18 18:07:51 +0100

    Change logger

--- src/server/logger.js
+++ src/server/logger.js
@@ -4,23 +4,20 @@ var winston = require('winston');
 var config = require('../../config.json');
 var moment = require('moment');
 
-var transports = [], json = false;
+var transports = []
 
-if (process.env.NODE_ENV != 'staging') {
-  transports.push(new winston.transports.Console({
-    level: 'debug',
-    handleExceptions: true,
-    json: false,
-    colorize: true
-  }));
-  json = true;
-}
+transports.push(new winston.transports.Console({
+  level: 'error',
+  handleExceptions: true,
+  json: false,
+  colorize: true
+}));
 
 transports.push(new winston.transports.File({
   level: 'info',
   filename: config.path.logs + '/' + process.env.NODE_ENV + '.otr.' + moment().format('YYYY-MM-DD') + '.0.log',
   handleExceptions: true,
-  json: json,
+  json: true,
   maxsize: 5242880, //5MB
   colorize: false,
 }));
