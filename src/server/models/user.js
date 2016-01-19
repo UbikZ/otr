a0d4cfe24156dbbb37fa0289b260bdbfd6c687ee
Gabriel Malet <gabrielmalet@gmail.com>
2016-01-19 17:37:17 +0100

    CamelCase support

--- src/server/models/user.js
+++ src/server/models/user.js
@@ -5,7 +5,7 @@ var mongoose = require('mongoose');
 var schema = new mongoose.Schema({
   identity: {
     token: String,
-    ontime_token: String,
+    ontimeToken: String,
   },
   name: {
     username: {type: String, index: {unique: true}},
