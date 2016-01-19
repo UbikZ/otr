a40c7bf0df8a13c8c909acdfe74137030ca26569
Gabriel Malet <gabrielmalet@gmail.com>
2016-01-19 20:07:02 +0100

    CLean

--- app.js
+++ app.js
@@ -2,7 +2,6 @@
 
 var express = require('express');
 var config = require('./config.json');
-var argv = require('yargs').argv;
 var app = express();
 
 config.env.debug = !~['staging', 'production'].indexOf(process.env.NODE_ENV);
