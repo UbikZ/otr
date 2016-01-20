0b6973c5a4d9f976b80dca4d8c9fb4e2adbe3ac7
Gabriel Malet <gabrielmalet@gmail.com>
2016-01-20 10:29:22 +0100

    Fix quality

--- src/server/controllers/user.js
+++ src/server/controllers/user.js
@@ -1,7 +1,6 @@
 'use strict';
 
 var User = require('../models/user');
-var jwt = require("jsonwebtoken");
 var http = require('./helpers/http');
 
 module.exports.controller = function (app, config) {
@@ -16,12 +15,12 @@ module.exports.controller = function (app, config) {
       User.find({}).lean().exec(function (err, users) {
         if (err) {
           http.log(req, 'Internal error: get users', err);
-          http.response(res, 500, {}, "-1", err);
+          http.response(res, 500, {}, '-1', err);
         } else if (users) {
           http.response(res, 200, {users: users});
         } else {
           http.log(req, 'Error: users is undefined (criteria = ' + {} + ').');
-          http.response(res, 404, {}, "-12");
+          http.response(res, 404, {}, '-12');
         }
       });
     });
@@ -32,10 +31,10 @@ module.exports.controller = function (app, config) {
    */
   app.post(prefix + '/update', http.ensureAuthorized, function (req, res) {
     var data = req.body;
-    User.findOne({"identity.token": req.token}).lean().exec(function (err, user) {
+    User.findOne({'identity.token': req.token}).lean().exec(function (err, user) {
       if (err) {
         http.log(req, 'Internal error: update user', err);
-        http.response(res, 500, {}, "-1", err);
+        http.response(res, 500, {}, '-1', err);
       } else if (user) {
         if (data.firstname) {
           user.name.firstname = data.firstname;
@@ -52,16 +51,16 @@ module.exports.controller = function (app, config) {
         if (data.job) {
           user.info.job = data.job;
         }
-        User.update({_id: user._id}, user, {}).lean().exec(function (err, raw) {
+        User.update({_id: user._id}, user, {}).lean().exec(function (err) {
           if (err) {
             http.log(req, 'Internal error: update user -> save user', err);
-            http.response(res, 500, {}, "-1", err);
+            http.response(res, 500, {}, '-1', err);
           } else {
-            http.response(res, 200, {user: user}, "11");
+            http.response(res, 200, {user: user}, '11');
           }
         });
       } else {
-        http.response(res, 404, {}, "-12");
+        http.response(res, 404, {}, '-12');
       }
     });
   });
