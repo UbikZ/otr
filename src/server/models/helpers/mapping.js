76db325b91010abd369837a1ad442901aec54bd2
Gabriel Malet <gabrielmalet@gmail.com>
2016-01-20 11:59:00 +0100

    Improve quality

--- src/server/models/helpers/mapping.js
+++ src/server/models/helpers/mapping.js
@@ -3,64 +3,64 @@
 function settingDtoToDal(object, data) {
   var result = object || {};
 
-  if (data != undefined) {
-    if (typeof data.contributorPrice != 'undefined') {
+  if (data !== undefined) {
+    if (typeof data.contributorPrice !== 'undefined') {
       result.projectDev = result.projectDev || {};
       result.projectDev.contributorPrice = data.contributorPrice;
     }
-    if (typeof data.contributorOccupation != 'undefined') {
+    if (typeof data.contributorOccupation !== 'undefined') {
       result.projectDev = result.projectDev || {};
       result.projectDev.contributorOccupation = data.contributorOccupation;
     }
-    if (typeof data.scrummasterPrice != 'undefined') {
+    if (typeof data.scrummasterPrice !== 'undefined') {
       result.projectManagement = result.projectManagement || {};
       result.projectManagement.scrummasterPrice = data.scrummasterPrice;
     }
-    if (typeof data.scrummasterOccupation != 'undefined') {
+    if (typeof data.scrummasterOccupation !== 'undefined') {
       result.projectManagement = result.projectManagement || {};
       result.projectManagement.scrummasterOccupation = data.scrummasterOccupation;
     }
-    if (typeof data.showDev != 'undefined') {
+    if (typeof data.showDev !== 'undefined') {
       result.billing = result.billing || {};
       result.billing.showDevPrice = data.showDev;
     }
-    if (typeof data.rateMultiplier != 'undefined') {
+    if (typeof data.rateMultiplier !== 'undefined') {
       result.billing = result.billing || {};
       result.billing.rateMultiplier = data.rateMultiplier;
     }
-    if (typeof data.showManagement != 'undefined') {
+    if (typeof data.showManagement !== 'undefined') {
       result.billing = result.billing || {};
       result.billing.showManagementPrice = data.showManagement;
     }
-    if (typeof data.estimateType != 'undefined') {
+    if (typeof data.estimateType !== 'undefined') {
       result.unit = result.unit || {};
       result.unit.estimateType = data.estimateType;
     }
-    if (typeof data.rangeEstimateUnit != 'undefined') {
+    if (typeof data.rangeEstimateUnit !== 'undefined') {
       result.unit = result.unit || {};
       result.unit.rangeEstimateUnit = data.rangeEstimateUnit;
     }
-    if (typeof data.label != 'undefined') {
+    if (typeof data.label !== 'undefined') {
       result.unit = result.unit || {};
-      result.unit.label = data.label == "" ? null : data.label;
+      result.unit.label = data.label === '' ? null : data.label;
     }
-    if (typeof data.showDate != 'undefined') {
+    if (typeof data.showDate !== 'undefined') {
       result.date = result.date || {};
       result.date.show = data.showDate;
     }
-    if (typeof data.contributorAvailable != 'undefined') {
+    if (typeof data.contributorAvailable !== 'undefined') {
       result.iteration = result.iteration || {};
       result.iteration.contributorAvailable = data.contributorAvailable;
     }
-    if (typeof data.hourPerDay != 'undefined') {
+    if (typeof data.hourPerDay !== 'undefined') {
       result.iteration = result.iteration || {};
       result.iteration.hourPerDay = data.hourPerDay;
     }
-    if (typeof data.dayPerWeek != 'undefined') {
+    if (typeof data.dayPerWeek !== 'undefined') {
       result.iteration = result.iteration || {};
       result.iteration.dayPerWeek = data.dayPerWeek;
     }
-    if (typeof data.weekPerIteration != 'undefined') {
+    if (typeof data.weekPerIteration !== 'undefined') {
       result.iteration = result.iteration || {};
       result.iteration.weekPerIteration = data.weekPerIteration;
     }
@@ -71,4 +71,4 @@ function settingDtoToDal(object, data) {
 
 module.exports = {
   settingDtoToDal: settingDtoToDal,
-}
+};
--- src/server/models/helpers/utils.js
+++ src/server/models/helpers/utils.js
@@ -1,29 +1,32 @@
 'use strict';
 
 Array.prototype.id = function (elementId) {
-  var result = this.filter(function (obj) {
-    if (obj != undefined && obj._id == elementId) {
-      return obj;
-    }
-  }) || [];
+  var result =
+    this.filter(function (obj) {
+      /*jshint eqeqeq: false */
+      if (obj !== undefined && obj._id == elementId) {
+        /*jshint eqeqeq: true */
+        return obj;
+      }
+    }) || [];
 
-  return result.length == 1 ? result[0] : undefined;
+  return result.length === 1 ? result[0] : undefined;
 };
 
 function walkRecursively(element, cb) {
   var elements = [];
-  if (element != undefined) {
-    if (element.projects != undefined && element.projects.length > 0) {
+  if (element !== undefined) {
+    if (element.projects !== undefined && element.projects.length > 0) {
       elements.push.apply(elements, element.projects);
     }
-    if (element.documents != undefined && element.documents.length > 0) {
+    if (element.documents !== undefined && element.documents.length > 0) {
       elements.push.apply(elements, element.documents);
     }
-    if (element.versions != undefined && element.versions.length > 0) {
+    if (element.versions !== undefined && element.versions.length > 0) {
       elements.push.apply(elements, element.versions);
     }
 
-    if (elements.length == 0) {
+    if (elements.length === 0) {
       return;
     }
     elements.forEach(function (element) {
@@ -33,25 +36,25 @@ function walkRecursively(element, cb) {
   }
 }
 
-function findSpecificRecursivelyById(parentElement, elementId, cb, prevEl) {
+function findSpecificRecursivelyById(parentElement, elementId, cb) {
   var type;
-  if (parentElement.projects != undefined) {
+  if (parentElement.projects !== undefined) {
     var element = parentElement.projects.id(elementId);
     type = 'project';
-    if (element == undefined && parentElement.documents !== undefined) {
+    if ((element === undefined || element === null) && parentElement.documents !== undefined) {
       element = parentElement.documents.id(elementId);
       type = 'document';
-      if (element == undefined) {
-        parentElement.documents.forEach(function(document) {
-          if (document.versions != undefined) {
+      if (element === undefined || element === null) {
+        parentElement.documents.forEach(function (document) {
+          if (document.versions !== undefined) {
             element = document.versions.id(elementId);
             type = 'version';
           }
         });
       }
     }
-    if (element == undefined) {
-      if (parentElement.projects != undefined && parentElement.projects.length > 0) {
+    if (element === undefined || element === null) {
+      if (parentElement.projects !== undefined && parentElement.projects.length > 0) {
         parentElement.projects.forEach(function (subElement) {
           findSpecificRecursivelyById(subElement, elementId, cb, parentElement);
         });
