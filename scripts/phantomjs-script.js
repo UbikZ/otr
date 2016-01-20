f83145eda6ff0e85bc4a8c97fcd7d19f824dc0d4
Gabriel Malet <gabrielmalet@gmail.com>
2016-01-20 12:02:42 +0100

    Improve quality

--- scripts/phantomjs-script.js
+++ scripts/phantomjs-script.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 var webPage = require('webpage');
 var system = require('system');
@@ -18,21 +18,21 @@ var resourceWait = 300,
   debug = false;
 
 /**
- * Page Config
+ * Functions / Events
  */
 
-[page, pdfPage].forEach(function (cPage) {
-  cPage.onError = onError;
-  cPage.onResourceRequested = onResourceRequested;
-  cPage.onResourceReceived = onResourceReceived;
-});
-
-pdfPage.paperSize = {
-  format: "A4",
-  orientation: "portrait",
-  margin: {left: "0.5cm", right: "0.5cm", top: "1cm", bottom: "1cm"},
-};
-
+function open(currentPage, url, ownCallback) {
+  currentPage.open(url, function (status) {
+    if (status !== 'success') {
+      if (debug) {
+        console.log('Unable to load url');
+      }
+      phantom.exit(-1);
+    } else {
+      ownCallback();
+    }
+  });
+}
 
 function doRender() {
   var filePath = 'public/test.html';
@@ -45,50 +45,14 @@ function doRender() {
     page.content
       .replace(new RegExp('\\s*<script[^>]*>[\\s\\S]*?</script>\\s*', 'ig'), '')
       .replace(new RegExp('\\s*<div class="pace[^>]*>[\\s\\S]*?</div>\\s*', 'ig'), '')
-      .replace(new RegExp('\\s*<div id="wrapper-loader[^>]*>[\\s\\S]*?</div>\\s*', 'ig'), '')
-    , 'w');
+      .replace(new RegExp('\\s*<div id="wrapper-loader[^>]*>[\\s\\S]*?</div>\\s*', 'ig'), ''),
+    'w');
   page.close();
 
-  //pdfPage.clearMemoryCache();
   open(pdfPage, filePath, function () {
     pdfPage.render('public/exports/export.pdf');
     fs.remove(filePath);
     phantom.exit();
-  });
-}
-
-if (system.args.length < 2 || system.args.length > 3) {
-  console.log('Parameter missing');
-  phantom.exit(-1);
-} else {
-  url = system.args[1];
-  debug = system.args[2] == 1;
-
-  //page.clearMemoryCache();
-  open(page, url, function () {
-    forcedRenderTimeout = setTimeout(function () {
-      if (debug) {
-        console.log(count);
-      }
-      doRender();
-    }, maxRenderWait);
-  });
-}
-
-/**
- * Functions / Events
- */
-
-function open(currentPage, url, ownCallback) {
-  currentPage.open(url, function (status) {
-    if (status !== "success") {
-      if (debug) {
-        console.log('Unable to load url');
-      }
-      phantom.exit(-1);
-    } else {
-      ownCallback();
-    }
   });
 }
 
@@ -119,5 +83,40 @@ function onError(msg, trace) {
       console.log('  ', item.file, ':', item.line);
     });
   }
+}
+
+/**
+ * Page Config & Main
+ */
+
+[page, pdfPage].forEach(function (cPage) {
+  cPage.onError = onError;
+  cPage.onResourceRequested = onResourceRequested;
+  cPage.onResourceReceived = onResourceReceived;
+});
+
+pdfPage.paperSize = {
+  format: 'A4',
+  orientation: 'portrait',
+  margin: {left: '0.5cm', right: '0.5cm', top: '1cm', bottom: '1cm'},
+};
+
+if (system.args.length < 2 || system.args.length > 3) {
+  console.log('Parameter missing');
+  phantom.exit(-1);
+} else {
+  url = system.args[1];
+  /*jshint eqeqeq: false */
+  debug = system.args[2] == 1;
+  /*jshint eqeqeq: true */
+
+  open(page, url, function () {
+    forcedRenderTimeout = setTimeout(function () {
+      if (debug) {
+        console.log(count);
+      }
+      doRender();
+    }, maxRenderWait);
+  });
 }
 
