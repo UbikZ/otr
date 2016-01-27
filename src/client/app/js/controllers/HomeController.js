3d3266819f91f6d8853e70c1e0538fc8e1662ed1
Gabriel Malet <gabrielmalet@gmail.com>
2016-01-27 11:18:25 +0100

    Es6 HomeController

--- src/client/app/js/controllers/HomeController.js
+++ src/client/app/js/controllers/HomeController.js
@@ -1,10 +1,10 @@
 'use strict';
 
 class HomeController {
- constructor($rootScope) {
-   $rootScope.enableUi();
-  this.$inject = ['$rootScope'];
- }
+  constructor($rootScope) {
+    $rootScope.enableUi();
+    this.$inject = ['$rootScope'];
+  }
 }
 
 export default HomeController;
