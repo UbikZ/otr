32b98f073f737355651cb95a368c5d12f540e0f5
Gabriel Malet <gabrielmalet@gmail.com>
2016-01-29 18:00:42 +0100

    5 to 6 for organization scenario

--- src/server/test/scenarios/org.specs.js
+++ src/server/test/scenarios/org.specs.js
@@ -1,71 +1,66 @@
 'use strict';
 
-var assert = require('chai').assert;
-var mongoose = require('mongoose');
+const assert = require('chai').assert;
+const mongoose = require('mongoose');
 
-var helpers = require('./../helpers');
+const helpers = require('./../helpers');
 
 module.exports = function (agent, url) {
-  describe('> Organization API', function () {
-    var organizationId;
-    describe('# [POST] ' + url + '/organization/edit', function () {
-      it('should get an internal error on "findById" (mongo fail)', function (done) {
-        helpers.mockModel(mongoose.model('Organization'), 'findById', function (stub) {
+  describe('> Organization API', () => {
+    let organizationId;
+    describe('# [POST] ' + url + '/organization/edit', () => {
+      it('should get an internal error on "findById" (mongo fail)', done => {
+        helpers.mockModel(mongoose.model('Organization'), 'findById', stub => {
           agent
             .post(url + '/organization/edit')
             .set('Authorization', 'Bearer ' + global.tokenBearer + ' ' + global.tokenOtBearer)
             .expect(500)
             .expect('Content-Type', 'application/json; charset=utf-8')
-            .end(function (err, res) {
-              if (err) {
-                return done(err);
-              }
-              var result = res.body;
+            .then(res => {
+              const result = res.body;
               assert.strictEqual(result.code, 500);
               assert.isDefined(result.error);
               assert.strictEqual(result.messageCode, '-1');
               stub.restore();
               done();
-            });
+            })
+            .catch(err => done(err))
+          ;
         });
       });
 
-      it('should get an internal error on create (mongo fail)', function (done) {
-        var sentData = require('./../fixtures/organization/create');
-        helpers.mockModel(mongoose.model('Organization'), 'update', function (stub) {
+      it('should get an internal error on create (mongo fail)', done => {
+        const sentData = require('./../fixtures/organization/create');
+        helpers.mockModel(mongoose.model('Organization'), 'update', stub => {
           agent
             .post(url + '/organization/edit')
             .send(sentData)
             .set('Authorization', 'Bearer ' + global.tokenBearer + ' ' + global.tokenOtBearer)
             .expect(500)
             .expect('Content-Type', 'application/json; charset=utf-8')
-            .end(function (err, res) {
-              if (err) {
-                return done(err);
-              }
-              var result = res.body;
+            .then(res => {
+              const result = res.body;
               assert.strictEqual(result.code, 500);
               assert.isDefined(result.error);
               assert.strictEqual(result.messageCode, '-1');
               stub.restore();
               done();
-            });
+            })
+            .catch(err => done(err))
+          ;
         });
       });
 
-      it('should create new organization', function (done) {
-        var sentData = require('./../fixtures/organization/create');
+      it('should create new organization', done => {
+        const sentData = require('./../fixtures/organization/create');
         agent
           .post(url + '/organization/edit')
           .set('Authorization', 'Bearer ' + global.tokenBearer + ' ' + global.tokenOtBearer)
           .send(sentData)
           .expect(200)
           .expect('Content-Type', 'application/json; charset=utf-8')
-          .end(function (err, res) {
-            if (err) {
-              return done(err);
-            }
-            var result = res.body;
+          .then(res => {
+            const result = res.body;
             assert.strictEqual(result.code, 200);
             assert.isUndefined(result.error);
             assert.strictEqual(result.messageCode, '5');
@@ -81,35 +76,36 @@ module.exports = function (agent, url) {
             assert.isDefined(result.organization.update.user);
             organizationId = result.organization._id;
             done();
-          });
+          })
+          .catch(err => done(err))
+        ;
       });
 
-      it('should get an internal error on update (mongo fail)', function (done) {
-        var sentData = require('./../fixtures/organization/update');
+      it('should get an internal error on update (mongo fail)', done => {
+        const sentData = require('./../fixtures/organization/update');
         sentData._id = organizationId;
-        helpers.mockModel(mongoose.model('Organization'), 'update', function (stub) {
+        helpers.mockModel(mongoose.model('Organization'), 'update', stub => {
           agent
             .post(url + '/organization/edit')
             .send(sentData)
             .set('Authorization', 'Bearer ' + global.tokenBearer + ' ' + global.tokenOtBearer)
             .expect(500)
             .expect('Content-Type', 'application/json; charset=utf-8')
-            .end(function (err, res) {
-              if (err) {
-                return done(err);
-              }
-              var result = res.body;
+            .then(res => {
+              const result = res.body;
               assert.strictEqual(result.code, 500);
               assert.isDefined(result.error);
               assert.strictEqual(result.messageCode, '-1');
               stub.restore();
               done();
-            });
+            })
+            .catch(err => done(err))
+          ;
         });
       });
 
-      it('should update organization', function (done) {
-        var sentData = require('./../fixtures/organization/update');
+      it('should update organization', done => {
+        const sentData = require('./../fixtures/organization/update');
         sentData._id = organizationId;
         agent
           .post(url + '/organization/edit')
@@ -117,11 +113,8 @@ module.exports = function (agent, url) {
           .send(sentData)
           .expect(200)
           .expect('Content-Type', 'application/json; charset=utf-8')
-          .end(function (err, res) {
-            if (err) {
-              return done(err);
-            }
-            var result = res.body;
+          .then(res => {
+            const result = res.body;
             assert.strictEqual(result.code, 200);
             assert.isUndefined(result.error);
             assert.strictEqual(result.messageCode, '6');
@@ -137,11 +130,13 @@ module.exports = function (agent, url) {
             assert.isDefined(result.organization.update);
             assert.isDefined(result.organization.update.user);
             done();
-          });
+          })
+          .catch(err => done(err))
+        ;
       });
 
-      it('should update organization (with lazy loading)', function (done) {
-        var sentData = require('./../fixtures/organization/update');
+      it('should update organization (with lazy loading)', done => {
+        const sentData = require('./../fixtures/organization/update');
         sentData._id = organizationId;
         sentData.lazy = 1;
         agent
@@ -150,11 +145,8 @@ module.exports = function (agent, url) {
           .send(sentData)
           .expect(200)
           .expect('Content-Type', 'application/json; charset=utf-8')
-          .end(function (err, res) {
-            if (err) {
-              return done(err);
-            }
-            var result = res.body;
+          .then(res => {
+            const result = res.body;
             assert.strictEqual(result.code, 200);
             assert.isUndefined(result.error);
             assert.strictEqual(result.messageCode, '6');
@@ -170,84 +162,80 @@ module.exports = function (agent, url) {
             assert.isDefined(result.organization.update.user);
             assert.isUndefined(result.organization.projects);
             done();
-          });
+          })
+          .catch(err => done(err))
+        ;
       });
     });
 
-    describe('# [GET] ' + url + '/organization', function () {
-      it('should get an internal error (mongo fail)', function (done) {
-        helpers.mockModel(mongoose.model('Organization'), 'find', function (stub) {
+    describe('# [GET] ' + url + '/organization', () => {
+      it('should get an internal error (mongo fail)', done => {
+        helpers.mockModel(mongoose.model('Organization'), 'find', stub => {
           agent
             .get(url + '/organization')
             .set('Authorization', 'Bearer ' + global.tokenBearer + ' ' + global.tokenOtBearer)
             .expect(500)
             .expect('Content-Type', 'application/json; charset=utf-8')
-            .end(function (err, res) {
-              if (err) {
-                return done(err);
-              }
-              var result = res.body;
+            .then(res => {
+              const result = res.body;
               assert.strictEqual(result.code, 500);
               assert.isDefined(result.error);
               assert.strictEqual(result.messageCode, '-1');
               stub.restore();
               done();
-            });
+            })
+            .catch(err => done(err))
+          ;
         });
       });
 
-      it('should get an empty array (unknown organizationId : not found)', function (done) {
+      it('should get an empty array (unknown organizationId : not found)', done => {
         agent
           .get(url + '/organization?id=56961966de7cbad8ba3be46d')
           .set('Authorization', 'Bearer ' + global.tokenBearer + ' ' + global.tokenOtBearer)
           .expect(200)
           .expect('Content-Type', 'application/json; charset=utf-8')
-          .end(function (err, res) {
-            if (err) {
-              return done(err);
-            }
-            var result = res.body;
+          .then(res => {
+            const result = res.body;
             assert.strictEqual(result.code, 200);
             assert.isUndefined(result.error);
             assert.isUndefined(result.messageCode);
             assert.isArray(result.organizations);
             assert.strictEqual(result.organizations.length, 0);
             done();
-          });
+          })
+          .catch(err => done(err))
+        ;
       });
 
-      it('should get an error 404 not found (query issue)', function (done) {
-        helpers.mockModel(mongoose.model('Organization'), 'find', function (stub) {
+      it('should get an error 404 not found (query issue)', done => {
+        helpers.mockModel(mongoose.model('Organization'), 'find', stub => {
           agent
             .get(url + '/organization?id=' + organizationId)
             .set('Authorization', 'Bearer ' + global.tokenBearer + ' ' + global.tokenOtBearer)
             .expect(404)
             .expect('Content-Type', 'application/json; charset=utf-8')
-            .end(function (err, res) {
-              if (err) {
-                return done(err);
-              }
-              var result = res.body;
+            .then(res => {
+              const result = res.body;
               assert.strictEqual(result.code, 404);
               assert.isUndefined(result.error);
               assert.strictEqual(result.messageCode, '-9');
               stub.restore();
               done();
-            });
+            })
+            .catch(err => done(err))
+          ;
         }, true);
       });
 
-      it('should request one organization', function (done) {
+      it('should request one organization', done => {
         agent
           .get(url + '/organization?id=' + organizationId)
           .set('Authorization', 'Bearer ' + global.tokenBearer + ' ' + global.tokenOtBearer)
           .expect(200)
           .expect('Content-Type', 'application/json; charset=utf-8')
-          .end(function (err, res) {
-            if (err) {
-              return done(err);
-            }
-            var result = res.body;
+          .then(res => {
+            const result = res.body;
             assert.strictEqual(result.code, 200);
             assert.isUndefined(result.error);
             assert.isUndefined(result.messageCode);
@@ -255,21 +243,20 @@ module.exports = function (agent, url) {
             assert.strictEqual(result.organizations.length, 1);
             assert.isArray(result.organizations[0].projects);
             done();
-          });
+          })
+          .catch(err => done(err))
+        ;
       });
 
 
-      it('should request list of all organizations (without lazy loading)', function (done) {
+      it('should request list of all organizations (without lazy loading)', done => {
         agent
           .get(url + '/organization?')
           .set('Authorization', 'Bearer ' + global.tokenBearer + ' ' + global.tokenOtBearer)
           .expect(200)
           .expect('Content-Type', 'application/json; charset=utf-8')
-          .end(function (err, res) {
-            if (err) {
-              return done(err);
-            }
-            var result = res.body;
+          .then(res => {
+            const result = res.body;
             assert.strictEqual(result.code, 200);
             assert.isUndefined(result.error);
             assert.isUndefined(result.messageCode);
@@ -277,20 +264,19 @@ module.exports = function (agent, url) {
             assert.strictEqual(result.organizations.length, 1);
             assert.isArray(result.organizations[0].projects);
             done();
-          });
+          })
+          .catch(err => done(err))
+        ;
       });
 
-      it('should request list of all organizations (with lazy loading)', function (done) {
+      it('should request list of all organizations (with lazy loading)', done => {
         agent
           .get(url + '/organization?lazy=1')
           .set('Authorization', 'Bearer ' + global.tokenBearer + ' ' + global.tokenOtBearer)
           .expect(200)
           .expect('Content-Type', 'application/json; charset=utf-8')
-          .end(function (err, res) {
-            if (err) {
-              return done(err);
-            }
-            var result = res.body;
+          .then(res => {
+            const result = res.body;
             assert.strictEqual(result.code, 200);
             assert.isUndefined(result.error);
             assert.isUndefined(result.messageCode);
@@ -298,50 +284,50 @@ module.exports = function (agent, url) {
             assert.strictEqual(result.organizations.length, 1);
             assert.isUndefined(result.organizations[0].projects);
             done();
-          });
+          })
+          .catch(err => done(err))
+        ;
       });
     });
 
-    describe('# [DELETE] ' + url + '/organization/delete', function () {
-      it('should get an internal error (mongo fail)', function (done) {
-        helpers.mockModel(mongoose.model('Organization'), 'findByIdAndRemove', function (stub) {
+    describe('# [DELETE] ' + url + '/organization/delete', () => {
+      it('should get an internal error (mongo fail)', done => {
+        helpers.mockModel(mongoose.model('Organization'), 'findByIdAndRemove', stub => {
           agent
             .post(url + '/organization/delete')
             .send({id: organizationId})
             .set('Authorization', 'Bearer ' + global.tokenBearer + ' ' + global.tokenOtBearer)
             .expect(500)
             .expect('Content-Type', 'application/json; charset=utf-8')
-            .end(function (err, res) {
-              if (err) {
-                return done(err);
-              }
-              var result = res.body;
+            .then(res => {
+              const result = res.body;
               assert.strictEqual(result.code, 500);
               assert.isDefined(result.error);
               assert.strictEqual(result.messageCode, '-1');
               stub.restore();
               done();
-            });
+            })
+            .catch(err => done(err))
+          ;
         });
       });
 
-      it('should delete one organization', function (done) {
+      it('should delete one organization', done => {
         agent
           .post(url + '/organization/delete')
           .send({id: organizationId})
           .set('Authorization', 'Bearer ' + global.tokenBearer + ' ' + global.tokenOtBearer)
           .expect(200)
           .expect('Content-Type', 'application/json; charset=utf-8')
-          .end(function (err, res) {
-            if (err) {
-              return done(err);
-            }
-            var result = res.body;
+          .then(res => {
+            const result = res.body;
             assert.strictEqual(result.code, 200);
             assert.isUndefined(result.error);
             assert.strictEqual(result.messageCode, '7');
             done();
-          });
+          })
+          .catch(err => done(err))
+        ;
       });
     });
   });
