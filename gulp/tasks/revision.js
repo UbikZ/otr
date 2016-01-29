f4e68dffcc06540ffc9bd6a6ac4717a7dee714d2
Gabriel Malet <gabrielmalet@gmail.com>
2016-01-29 12:04:30 +0100

    es52es6

--- gulp/tasks/revision.js
+++ gulp/tasks/revision.js
@@ -1,25 +1,33 @@
 'use strict';
 
-var fs = require('fs');
-var merge = require('merge');
+import fs from 'fs';
+import merge from 'merge';
 
-module.exports = function (gulp, plugins, npmPackages, config) {
-  return function () {
-    fs.readFile(config.path.client.app + '/views/index.html', 'utf8', function (err, data) {
-      if (err) {
-        return console.log(err);
-      }
-      if (!config.env.debug) {
-        var manifest = {};
-        ['app', 'vendor', 'css', 'print'].forEach(function (el) {
-          manifest = merge(manifest, require('../../' + config.path.public + '/dist/rev-manifest.' + el + '.json'));
-        });
-        Object.keys(manifest).forEach(element => {
-          data = data.replace(element, manifest[element]);
-        }, manifest);
-      }
+/**
+ * Task to add revision on each static (js && css) files if production or staging
+ * @param gulp
+ * @param plugins
+ * @param npmPackages
+ * @param config
+ * @returns {Function}
+ */
+export default (gulp, plugins, npmPackages, config) => {
+  return () => {
+      fs.readFile(config.path.client.app + '/views/index.html', 'utf8', (err, data) => {
+        if (err) {
+          return console.log(err);
+        }
+        if (!config.env.debug) {
+          let manifest = {};
+          ['app', 'vendor', 'css', 'print'].forEach(function (el) {
+            manifest = merge(manifest , require('../' + config.path.public + '/dist/rev-manifest.' + el + '.json'));
+          });
+          Object.keys(manifest).forEach(element => {
+            data = data.replace(element, manifest[element]);
+          }, manifest);
+        }
 
-      fs.writeFile(config.path.public + '/index.html', data);
-    });
+        fs.writeFile(config.path.public + '/index.html', data);
+      });
   };
 };
