601ec71c6956cbd5f82693a0e2bd2b995b064242
Gabriel Malet <gabrielmalet@gmail.com>
2016-01-29 17:41:15 +0100

    5to6 for init scenario

--- src/server/test/scenarios/init.specs.js
+++ src/server/test/scenarios/init.specs.js
@@ -6,43 +6,39 @@ var mongoose = require('mongoose');
 
 var helpers = require('./../helpers');
 
-module.exports = function (agent, url, config) {
+module.exports = (agent, url, config) => {
   var tokenBearer, tokenOtBearer;
 
-  describe('> Init API', function () {
-    describe('# [GET] /', function () {
-      it('returns index.html', function (done) {
+  describe('> Init API', () => {
+    describe('# [GET] /', () => {
+      it('returns index.html', done => {
         agent
           .get('/')
           .expect(200)
           .expect('Content-Type', 'text/html; charset=UTF-8')
-          .end(function (err, res) {
-            if (err) {
-              return done(err);
-            }
+          .then(res => {
             res.text.should.be.a('string');
             done();
-          });
+          })
+          .catch(err => done(err))
+        ;
       });
     });
 
-    describe('# [GET] /dist/*.gz', function () {
-      var path = config.path.public + '/dist';
-      var staticFiles = [];
+    describe('# [GET] /dist/*.gz', () => {
+      const path = config.path.public + '/dist';
+      let staticFiles = [];
 
-      it('check public/dist exists', function (done) {
+      it('check public/dist exists', done => {
         assert.isTrue(fs.existsSync(path));
-        staticFiles = fs.readdirSync(path).filter(function (file) {
-          return ~file.indexOf('.gz.');
-        });
+        staticFiles = fs.readdirSync(path).filter(file => ~file.indexOf('.gz.'));
         done();
       });
 
-
-      it('returns *.gz compiled files', function (done) {
-        var isDone = 0;
-        staticFiles.forEach(function (staticFile) {
-          var contentType;
+      it('returns *.gz compiled files', done => {
+        let isDone = 0;
+        staticFiles.forEach(staticFile => {
+          let contentType;
           if (~staticFile.indexOf('.js')) {
             contentType = 'application/javascript';
           } else if (~staticFile.indexOf('.css')) {
@@ -53,73 +49,69 @@ module.exports = function (agent, url, config) {
             .expect(200)
             .expect('Content-Encoding', 'gzip')
             .expect('Content-Type', contentType)
-            .end(function (err, res) {
-              if (err) {
-                return done(err);
-              }
+            .then(res => {
               res.text.should.be.a('string');
               if (++isDone === staticFiles.length) {
                 done();
               }
-            });
+            })
+            .catch(err => done(err))
+          ;
         });
       });
     });
 
-    describe('# [GET] ' + url + '/user (examples)', function () {
-      it('returns a 403 (Not Allowed) error (without Bearer Token)', function (done) {
+    describe('# [GET] ' + url + '/user (examples)', () => {
+      it('returns a 403 (Not Allowed) error (without Bearer Token)', done => {
         agent
           .get(url + '/user')
           .expect(403)
           .expect('Content-Type', 'application/json; charset=utf-8')
-          .end(function (err, res) {
-            if (err) {
-              return done(err);
-            }
-            var result = res.body;
+          .then(res => {
+            const result = res.body;
             assert.strictEqual(result.code, 403);
             assert.strictEqual(result.messageCode, '-3');
             done();
-          });
+          })
+          .catch(err => done(err))
+        ;
       });
 
-      it('returns an internal error (checkAuthorized fail)', function (done) {
-        helpers.mockModel(mongoose.model('User'), 'findOne', function (stub) {
+      it('returns an internal error (checkAuthorized fail)', done => {
+        helpers.mockModel(mongoose.model('User'), 'findOne', stub => {
           agent
             .get(url + '/user')
             .set('Authorization', 'Bearer ' + tokenBearer + ' ' + tokenOtBearer)
             .expect(500)
             .expect('Content-Type', 'application/json; charset=utf-8')
-            .end(function (err, res) {
-              if (err) {
-                return done(err);
-              }
-              var result = res.body;
+            .then(res => {
+              const result = res.body;
               assert.strictEqual(result.code, 500);
               assert.strictEqual(result.messageCode, '-1');
               assert.isDefined(result.error);
               stub.restore();
               done();
-            });
+            })
+            .catch(err => done(err))
+          ;
         });
       });
 
-      it('returns an internal error (checkAuthorized with unknown token)', function (done) {
+      it('returns an internal error (checkAuthorized with unknown token)', done => {
         agent
           .get(url + '/user')
           .set('Authorization', 'Bearer 569a498efd2e11a55a2822f4 ' + tokenOtBearer)
           .expect(404)
           .expect('Content-Type', 'application/json; charset=utf-8')
-          .end(function (err, res) {
-            if (err) {
-              return done(err);
-            }
-            var result = res.body;
+          .then(res => {
+            const result = res.body;
             assert.strictEqual(result.code, 404);
             assert.strictEqual(result.messageCode, '-2');
             assert.isUndefined(result.error);
             done();
-          });
+          })
+          .catch(err => done(err))
+        ;
       });
     });
   });
